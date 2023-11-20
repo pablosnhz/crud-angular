868b1a22743330131683f36a95a44090eb95993e
PabloSnhz <pablosanhuezaaa@gmail.com>
2023-11-20 20:35:13 -0300

    empleados

--- crud-con-angular/src/app/app.component.ts
+++ crud-con-angular/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { empleado } from './models/empleado/empleado.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud-con-angular';
+  empleadoArray: empleado[] = [
+    {id: 1, name: 'Morty', country: 'USA'},
+    {id: 2, name: 'Rick', country: 'Colombia'},
+    {id: 3, name: 'Summer', country: 'Madagascar'}
+
+
+  ]
 }
---
+++ crud-con-angular/src/app/models/empleado/empleado.component.ts
@@ -0,0 +1,5 @@
+export class empleado {
+  id: number = 0;
+  name: string = '';
+  country: string = '';
+}
